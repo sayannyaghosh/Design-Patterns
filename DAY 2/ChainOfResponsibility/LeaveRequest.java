a1d00586a01ad5596f46830f7e0f8f3c338f7d92
Sayannya Ghosh <42893577+sayannyaghosh@users.noreply.github.com>
2021-03-19 21:24:28 +0530

    Create LeaveRequest.java

---
+++ DAY 2/ChainOfResponsibility/LeaveRequest.java
@@ -0,0 +1,22 @@
+package chainOfResponsiblity;
+
+public class LeaveRequest {
+
+	private String employeeName;
+	private int leaveDays;
+
+	
+	
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+
+	public void setLeaveDays(int leaveDays) {
+		this.leaveDays = leaveDays;
+	}
