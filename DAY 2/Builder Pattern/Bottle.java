277f2eb0ec60b0157d1374f68e9e0c5577e69688
Sayannya Ghosh <42893577+sayannyaghosh@users.noreply.github.com>
2021-03-19 11:47:50 +0530

    Update Bottle.java

--- DAY 2/Builder Pattern/Bottle.java
+++ DAY 2/Builder Pattern/Bottle.java
@@ -1,1 +1,12 @@
+package BuilderPattern;
 
+public class Bottle implements Packing{
+
+	@Override
+	public String pack() {
+		// TODO Auto-generated method stub
+		return "Bottle";
+	}
+
+}
+© 2021 GitHub, Inc.
