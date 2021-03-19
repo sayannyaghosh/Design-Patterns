0f1a21672e5b1b2548db4b48e93ceaea11acf821
Sayannya Ghosh <42893577+sayannyaghosh@users.noreply.github.com>
2021-03-19 12:04:32 +0530

    Create Bugativeron.java

---
+++ DAY 2/Adapter Pattern/Bugativeron.java
@@ -0,0 +1,16 @@
+package adapterPattern;
+
+public class Bugativeron implements Movable{
+	 
+	public double getSpeed() {
+		return 268;
+		
+	}
+
+	@Override
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		return 30;
+	}
+
+}
