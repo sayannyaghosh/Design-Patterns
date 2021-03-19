89340f4370ff935d12f4c499de544b99b336b9ca
Sayannya Ghosh <42893577+sayannyaghosh@users.noreply.github.com>
2021-03-19 11:52:44 +0530

    Create Coke.java

---
+++ DAY 2/Builder Pattern/Coke.java
@@ -0,0 +1,17 @@
+package BuilderPattern;
+
+public class Coke extends CoolDrinks{
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return "coke";
+	}
+
+	@Override
+	public float price() {
+		// TODO Auto-generated method stub
+		return 30.0f;
+	}
+
+}
