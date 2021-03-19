76f7de2af5503458bc6de4b9883f7cba613afcd5
Sayannya Ghosh <42893577+sayannyaghosh@users.noreply.github.com>
2021-03-19 21:33:01 +0530

    Create PremiumUser.java

---
+++ DAY 3/Mediator Pattern/PremiumUser.java
@@ -0,0 +1,26 @@
+//package mediatorPattern;
+//
+//public class PremiumUser implements User{
+//	
+//	private ChatMediator chat;
+//	private String name;
+//	
+//	
+//	public PremiumUser(String name) {
+//		this.name=name;
+//	}
+//
+//	@Override
+//	public void RecieveMessage(String msg) {
+//
+//		System.out.println(name+" "+msg);
+//		
+//	}
+//
+//	@Override
+//	public void SendMessage(String msg) {
+//
+//		chat.sendMessage(msg);
+//	}
+//
+//}
