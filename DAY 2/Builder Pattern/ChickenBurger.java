b4536490d7ea8c963edf48ad3a499dfd83774a28
Sayannya Ghosh <42893577+sayannyaghosh@users.noreply.github.com>
2021-03-19 11:51:58 +0530

    Create ChickenBurger.java

---
+++ DAY 2/Builder Pattern/ChickenBurger.java
@@ -0,0 +1,17 @@
+package BuilderPattern;
+
+public class ChickenBurger extends Burger{
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return "Non-Veg Burger";
+	}
+
+	@Override
+	public float price() {
+		// TODO Auto-generated method stub
+		return 50.5f;
+	}
+
+}
